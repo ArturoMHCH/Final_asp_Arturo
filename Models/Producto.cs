358a4308350d1f0034d885ebc07c16b8d80470e9
ArturoMHCH <mscaljrokline@gmail.com>
2022-10-21 19:46:29 -0500

    entidad producto

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,6 +2,10 @@
 {
     public class Producto
     {
-        //
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public double precio { get; set; }
+        public string imagen { get; set; }
+
     }
 }
