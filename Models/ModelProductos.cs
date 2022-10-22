a1ac988985da398f726b8f97b0b89e20193d5e41
ArturoMHCH <mscaljrokline@gmail.com>
2022-10-22 08:48:17 -0500

    Clase intermedia Model

---
+++ Models/ModelProductos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FINALASPNET.Models
+{
+    public class ModelProductos
+    {
+        private List<Producto> productos;
+        public ModelProductos()
+        {
+            productos = new List<Producto> {
+                new Producto{
+                    id = 1,
+                    nombre = "Atún VanCamps",
+                    precio = 12,
+                    imagen = "atun.jpg"
+                },
+                new Producto{
+                    id = 2,
+                    nombre = "Queso menonita",
+                    precio = 45,
+                    imagen = "queso.jpg"
+                }
+            };
+        }
+
+        public List<Producto> getTodo()
+        {
+            return productos;
+        }
+        public Producto getProducto(int id)
+        {
+            return productos.Single(p => p.id == id);
+        }
+
+    }
+}
