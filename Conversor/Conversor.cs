598ec43e805e246c3638ad202edbad5af0139b86
ArturoMHCH <mscaljrokline@gmail.com>
2022-10-22 08:30:42 -0500

    Clase conversor con su carpeta

---
+++ Conversor/Conversor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace FINALASPNET.Conversor
+{
+    public static class Conversor
+    {
+        public static void GuardarObjeto(
+            this ISession sesion, string llave, object valor)
+        {
+            sesion.SetString(llave, JsonConvert.SerializeObject(valor));
+        }
+        public static T RecuperarObjeto<T>(
+            this ISession sesion, string llave)
+        {
+            var valor = sesion.GetString(llave);
+            return valor == null ? default(T)
+                : JsonConvert.DeserializeObject<T>(valor);
+        }
+    }
+
+}
