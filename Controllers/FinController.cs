ef31137717a2798cb7975df5032182d35958613e
ArturoMHCH <mscaljrokline@gmail.com>
2022-10-23 14:27:32 -0500

    Añadiendo controlador de fin de compra

---
+++ Controllers/FinController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FINALASPNET.Controllers
+{
+    public class FinController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
